4ce611fe5451bf6107ac738a7e54778ce36e6ad6
unknown <Administrator@s20203214.campus.tue.nl>
2022-05-04 18:43:37 +0200

    this code replaces the pruncated text with the full text, version 2

--- clean.py
+++ clean.py
@@ -1,21 +1,7 @@
 import index
 tweet = index.tweets
 
-def number_of_language(lang:str):
-    """counts number of tweets with a certain language"""
-    results = tweet.count_documents({"lang" : lang})
-    return print(results)
-
-#number_of_language("nl")
-
 results = tweet.find({"truncated": True})
-
-#for result in results:
-#    print(result["id"])
-
-#for result in results:
-#    if result['truncated']:
-#        print(result["extended_tweet"]["full_text"])
 
 for result in results:
     tweet.update_one({"id" : result["id"]}, {"$set" : {"text" : result["extended_tweet"]["full_text"]}})
