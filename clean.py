57ae68960fdc50ea682c5a4feb856d6e33f82303
unknown <Administrator@s20203214.campus.tue.nl>
2022-05-05 09:40:37 +0200

    new clean file to replace truncated tweets for full text

--- clean.py
+++ clean.py
@@ -1,7 +1,12 @@
-import index
-tweet = index.tweets
+import index #import other file
+tweet = index.tweets #get tweets variable from index.py
 
-results = tweet.find({"truncated": True})
+truncated = tweet.find({"truncated": True}) #find tweets that are truncated
+for object in truncated: #loop over all the tweets and replace the truncated tweet with the full text one
+    tweet.update_one({"id" : object["id"]}, {"$set" : {"text" : object["extended_tweet"]["full_text"]}})
 
-for result in results:
-    tweet.update_one({"id" : result["id"]}, {"$set" : {"text" : result["extended_tweet"]["full_text"]}})
+#The same, but for truncated tweets in retweeted status:
+truncated_retweets = tweet.find({"retweeted_status": {"$exists": True}}) 
+for object in truncated_retweets:
+    if object['retweeted_status']['truncated']:
+        tweet.update_one({"id" : object["id"]}, {"$set" : {"text" : object["retweeted_status"]["extended_tweet"]["full_text"]}})
