647c63afa0c59b3db3bdcf8cd688d601689bd0d4
Žygimantas Krasauskas <zygimantaskrasauskas@wlan-237217.nbw.tue.nl>
2022-05-09 15:41:31 +0200

    fixed data cleaning code

--- Data Cleaning/calling_functions.py
+++ Data Cleaning/calling_functions.py
@@ -1,6 +1,6 @@
 import cleaning_and_presentation_functions
 
-cleaning_and_presentation_functions.data_preparation()
+# cleaning_and_presentation_functions.data_preparation()
 cleaning_and_presentation_functions.tweet_object_cleaning()
 cleaning_and_presentation_functions.user_object_cleaning()
 cleaning_and_presentation_functions.retweeted_status_user_object_cleaning()
@@ -10,3 +10,4 @@ cleaning_and_presentation_functions.retweeted_quoted_status_cleaning()
 cleaning_and_presentation_functions.quoted_status_user_cleaning()
 cleaning_and_presentation_functions.quoted_status_cleaning()
 cleaning_and_presentation_functions.place_object_cleaning()
+cleaning_and_presentation_functions.entities_cleaning()
