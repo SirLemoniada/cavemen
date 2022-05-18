5d877c93157bc7586cd55f4671a01a966fb53f02
ChrisWoycik <k.j.wojcik@gmail.com>
2022-05-18 13:40:38 +0200

    idk what

--- grouping.py
+++ grouping.py
@@ -3,24 +3,30 @@ from unittest import result
 import pymongo
 from bson.regex import Regex
 import pandas as pd
-from dateutil.parser import parse
 import datetime
 import pprint
-from collections import OrderedDict
 from pandas import DataFrame
 
 import index #import index.py file
 tweet = index.tweets #import tweets variable from index.py file and assign to tweet
 import matplotlib.pyplot as plt
 
+screen_names = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin", "AirBerlin assist", 
+"easyJet"," RyanAir","SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
+
 for_plot = tweet.aggregate([
     #{ "$unwind": "$user.screen_name" },
    {"$match": {"entities.user_mentions.id":56377143}},
-   #{"$group" : {"_id":{"user":"$user.screen_name", 'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}, 'hour':{"$hour":"$created_at"}},"count": {"$sum":1}}},
-   {"$group" : {"_id":"$lang", "count": {"$sum":1}}},
+   {"$group" : {"_id":{"user":"$user.screen_name", 'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}, 'hour':{"$hour":"$created_at"}},"count": {"$sum":1}}},
+   #{"$group" : {"_id":"$lang", "count": {"$sum":1}}},
    {"$sort": {"count": -1}},
-   #{"$project": {"lang":1, "_id":1, "created_at":1, "user.screen_name":1}},
    {"$limit":5}
 ])
-for doc in for_plot:
-    pprint.pprint(doc)
+
+list_cursor = list(for_plot)
+df = DataFrame(list_cursor)
+df2 = df.set_index("_id")
+print(df2.head())
+
+# for doc in for_plot: #iterating through commandcursor object
+#     pprint.pprint(doc)
