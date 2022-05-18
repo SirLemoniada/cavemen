206fc37c09d56ac8a0aeff8f39a5dcb1d2efdaee
ChrisWoycik <k.j.wojcik@gmail.com>
2022-05-18 21:14:47 +0200

    jumbo

--- clean_for_aggregation.py
+++ clean_for_aggregation.py
@@ -18,27 +18,11 @@ import matplotlib.pyplot as plt
 screen_names = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin", "AirBerlin assist", 
 "easyJet"," RyanAir","SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
 
-#1 removing empty time strings
-#2 changing time to timestamp
-#3 creating aggregation - groupby time, airline, and count()
-#4 plotting
 
-#for twt in tweet.find({}):
-#    tweet.delete_one({"created_at": {"$exists": False}})
-
-
-#print(tweet.count_documents({"created_at": {"$exists": False}}))
 def remove_wrong_time():
-    for twt in tweet.find({}):
-        tweet.delete_one({"created_at": {"$exists": False} })
+    tweet.delete_many({"created_at": {"$exists": False} })
     return
 
-    #     outliers = tweet.find({"id": {"$exists" : False}}) # Find all values that do not have an id
-    # for error_tweets in outliers: # Iterates through outliers and deletes them
-    #     tweet.delete_one({"_id" : error_tweets["_id"]})
-
-
-#date = datetime.datetime.strptime("30 Nov 00", '%a %b %d %X %z %Y')
 
 
 def time_to_timestamp():
@@ -52,46 +36,11 @@ def time_to_timestamp():
     return
 
 
-# t = tweet.find_one({})
-# k = t['created_at'].year
-# print(k)
-
-# def most_tweets():
-#     new_json = tweet.aggregate([
-#     {"$group" : {"_id":{"$user.screen_name"}, "count": {"$sum":1}}}
-#     ])
-# if __name__ == '__main__':
-#     new_json = most_tweets()
-#     pprint.pprint(new_json)
-# {"screen_name":"$screen_name", "created_at":"$created_at"}
-for_plot = tweet.aggregate([
-    #{ "$unwind": "$user.screen_name" },
-   {"$match": {"user.screen_name":{"$in":screen_names}}},
-   #{"$group" : {"_id":{"user":"$user.screen_name", 'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}, 'hour':{"$hour":"$created_at"}},"count": {"$sum":1}}},
-   {"$group" : {"_id":"$user.screen_name", "count": {"$sum":1}}},
-   {"$sort": {"_id": 1}},
-   #{"$project": {"lang":1, "_id":1, "created_at":1, "user.screen_name":1}},
-   {"$limit":5}
-])
-list_cursor = list(for_plot)
-df = DataFrame(list_cursor)
-df2 = df.set_index("_id")
-print(df2.head())
-# for doc in for_plot:
-#     pprint.pprint(doc)
-plt.bar(*zip(*df2.items()))
-plt.show()
 
 
-# cursor = tweet.aggregate([
-#    {"$match": {"user.screen_name":{"$in":["KLM", "British_Airways"]}}},
-#    {"$sort": OrderedDict([("user.screen_name", -1)])},
-#    {"$project": {"lang":1, "_id":0, "created_at":1, "user.screen_name":1}},
-#     {"$limit":3}
-# ])
-# for doc in cursor:
-#     pprint.pprint(doc)
-#print(tweet.count_documents({"created_at":{"$exists":True}}))
+
+
+
 
 
 
--- running.py
+++ running.py
@@ -1,4 +1,5 @@
 from time import time
-from airline_plot import remove_wrong_time, time_to_timestamp
-#remove_wrong_time()
-time_to_timestamp()
+from clean_for_aggregation import remove_wrong_time, time_to_timestamp 
+#run one by one, first remove_wrong_time then time_to_timestamp
+remove_wrong_time()
+#time_to_timestamp()
