7a8b07815ac202dc8acbbe907d22a6dd7e9d16e8
20210967 <d.lin1@student.tue.nl>
2022-05-30 21:15:36 +0200

    grouping conversations start with others for every airline

--- collections_function.py
+++ collections_function.py
@@ -1,3 +1,4 @@
+from unittest import skip
 import index #import index.py file
 tweet = index.tweets #import tweets variable from index.py file and assign to tweet
 
@@ -15,157 +16,146 @@ EtihadAirways = index.EtihadAirways_conversations
 VirginAtlantic = index.VirginAtlantic_conversations
 
 def KLM_conversation_start_with_others_function():
-    """Groups conversations that start with a random user, who mentions klm, klm responds to user and initial user responds to klm again (depth of 3).
-    also contains tweets were KLM is mentioned, but didn't react."""
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':56377143}):
-        KLM.insert_one(init_tweet) #insert all tweets that mention KLM
-        reply = tweet.find_one({'user.id':56377143, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            KLM.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}}) #inserts reply from KLM as child object
+    for depth_2 in tweet.find({'user.id':56377143, 'is_a_reply':True}):
 
-    for replies in KLM.find({"depth_2" : {"$exists" : True}}):
-        reply_to_KLM = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_KLM != None:
-            KLM.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_KLM}}) #inserts reply to reply as child object
-
-    # for replies in klm.find({"depth_3" : {"$exists" : True}}):
-    #     reply = tweet.find_one({'user.id':56377143, "in_reply_to_status_id":replies["depth_3"]["id"]})
-    #     if reply != None:
-    #         klm.update_one({'id' : replies["id"]}, {"$set" : {"depth_4" : reply}})  
-
-    # for replies in klm.find({"depth_4" : {"$exists" : True}}):
-    #     reply_to_KLM = tweet.find_one({"in_reply_to_status_id":replies["depth_4"]["id"]})
-    #     if reply_to_KLM != None:
-    #         klm.update_one({'id' : replies["id"]}, {"$set" : {"depth_5" : reply}})    
+        KLM.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            KLM.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                KLM.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            KLM.delete_one({'id' : depth_2['id']}) 
 
 def British_Airways_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':18332190}):
-        British_Airways.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':18332190, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            British_Airways.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in British_Airways.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            British_Airways.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':18332190, 'is_a_reply':True}):
+        British_Airways.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            British_Airways.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                British_Airways.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            British_Airways.delete_one({'id' : depth_2['id']}) 
 
 def AirFrance_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':106062176}):
-        AirFrance.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':106062176, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            AirFrance.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in AirFrance.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            AirFrance.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':106062176, 'is_a_reply':True}):
+        AirFrance.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            AirFrance.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                AirFrance.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            AirFrance.delete_one({'id' : depth_2['id']}) 
 
 def AmericanAir_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':22536055}):
-        AmericanAir.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':22536055, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            AmericanAir.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in AmericanAir.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            AmericanAir.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':22536055, 'is_a_reply':True}):
+        AmericanAir.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            AmericanAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                AmericanAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            AmericanAir.delete_one({'id' : depth_2['id']}) 
 
 def Lufthansa_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':124476322}):
-        Lufthansa.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':124476322, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            Lufthansa.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in Lufthansa.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            Lufthansa.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':124476322, 'is_a_reply':True}):
+        Lufthansa.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            Lufthansa.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                Lufthansa.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            Lufthansa.delete_one({'id' : depth_2['id']}) 
 
 def AirBerlin_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':26223583}):
-        AirBerlin.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':26223583, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            AirBerlin.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in AirBerlin.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            AirBerlin.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':26223583, 'is_a_reply':True}):
+        AirBerlin.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            AirBerlin.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                AirBerlin.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            AirBerlin.delete_one({'id' : depth_2['id']})
 
 def easyJet_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':38676903}):
-        easyJet.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':38676903, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            easyJet.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in easyJet.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            easyJet.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':38676903, 'is_a_reply':True}):
+        easyJet.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            easyJet.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                easyJet.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            easyJet.delete_one({'id' : depth_2['id']})
 
 def RyanAir_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':1542862735}):
-        RyanAir.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':1542862735, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            RyanAir.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in RyanAir.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            RyanAir.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':1542862735, 'is_a_reply':True}):
+        RyanAir.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            RyanAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                RyanAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            RyanAir.delete_one({'id' : depth_2['id']})
 
 def SingaporeAir_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':253340062}):
-        SingaporeAir.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':253340062, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            SingaporeAir.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in SingaporeAir.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            SingaporeAir.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':253340062, 'is_a_reply':True}):
+        SingaporeAir.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            SingaporeAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                SingaporeAir.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            SingaporeAir.delete_one({'id' : depth_2['id']})
 
 def Qantas_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':218730857}):
-        Qantas.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':218730857, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            Qantas.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in Qantas.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            Qantas.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':218730857, 'is_a_reply':True}):
+        Qantas.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            Qantas.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                Qantas.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            Qantas.delete_one({'id' : depth_2['id']})
 
 def EtihadAirways_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':45621423}):
-        EtihadAirways.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':45621423, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            EtihadAirways.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in EtihadAirways.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            EtihadAirways.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':45621423, 'is_a_reply':True}):
+        EtihadAirways.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            EtihadAirways.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                EtihadAirways.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            EtihadAirways.delete_one({'id' : depth_2['id']})
 
 def VirginAtlantic_conversation_start_with_others_function():
-    for init_tweet in tweet.find({'is_a_reply':False, 'entities.user_mentions.id':20626359}):
-        VirginAtlantic.insert_one(init_tweet) 
-        reply = tweet.find_one({'user.id':20626359, "in_reply_to_status_id":init_tweet['id']})
-        if reply != None:
-            VirginAtlantic.update_one({'id' : init_tweet['id']}, {"$set" : {"depth_2" : reply}})
-
-    for replies in VirginAtlantic.find({"depth_2" : {"$exists" : True}}):
-        reply_to_airline = tweet.find_one({"in_reply_to_status_id":replies["depth_2"]["id"]})
-        if reply_to_airline != None:
-            VirginAtlantic.update_one({'id' : replies["id"]}, {"$set" : {"depth_3" : reply_to_airline}})
+    for depth_2 in tweet.find({'user.id':20626359, 'is_a_reply':True}):
+        VirginAtlantic.insert_one(depth_2)
+        depth_1 = tweet.find_one({"id":depth_2['in_reply_to_status_id'], 'is_a_reply':False})
+        depth_3 = tweet.find_one({'user.id':depth_2['entities']['user_mentions'], "in_reply_to_status_id" : depth_2["id"]})
+        if (depth_1 != None) :
+            VirginAtlantic.update_one({'id' : depth_2['id']}, {"$set" : {"depth_1" : depth_1}})  
+            if (depth_3 != None) :
+                VirginAtlantic.update_one({'id' : depth_2['id']}, {"$set" : {"depth_3" : depth_3}})      
+        else:
+            VirginAtlantic.delete_one({'id' : depth_2['id']})
