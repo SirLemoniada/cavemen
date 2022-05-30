3bdf710f3e828ff713a033ca8474d70eb1e99816
20210967 <d.lin1@student.tue.nl>
2022-05-30 15:02:41 +0200

    none

--- index.py
+++ index.py
@@ -6,4 +6,15 @@ client = pymongo.MongoClient(conn_str)
 
 cavemen=client.cavemen
 tweets=cavemen.tweets
-klm_conversations=cavemen.klm
+KLM_conversations = cavemen.KLM
+British_Airways_conversations = cavemen.British_Airways 
+AirFrance_conversations = cavemen.AirFrance
+AmericanAir_conversations = cavemen.AmericanAir
+Lufthansa_conversations = cavemen.Lufthansa
+AirBerlin_conversations = cavemen.AirBerlin
+easyJet_conversations = cavemen.easyJet
+RyanAir_conversations = cavemen.RyanAir
+SingaporeAir_conversations = cavemen.SingaporeAir
+Qantas_conversations = cavemen.Qantas
+EtihadAirways_conversations = cavemen.EtihadAirways
+VirginAtlantic_conversations = cavemen.VirginAtlantic
