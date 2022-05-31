814a5244aa19ceb9b47f18983e201582e6a353f1
ChrisWoycik <k.j.wojcik@gmail.com>
2022-05-31 17:39:30 +0200

    airline plot ready

--- airline_plot.py
+++ airline_plot.py
@@ -19,25 +19,62 @@ screen_names = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa
 
 for_plot = tweet.aggregate([
     {"$match": {"user.screen_name":"KLM"}},
-   {"$group" : {"_id":{'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"},'hour':{"$hour":"$created_at"}},"count": {"$sum":1}}},
-   #{"$group" : {"_id":"$lang", "count": {"$sum":1}}},
-   #{ "$push": { "created_at": <value1>, ... } }
+   {"$group" : {"_id":{'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}},"count": {"$sum":1}}},
    {"$sort": {"_id": 1}},
    {"$project": {"_id": 0, "count":1}}
 ])
-#"user":"$user.screen_name"
-#'hour':{"$hour":"$created_at"}
-index = pd.date_range(start='1/1/2018', end='1/21/2018')
+for_plot2 = tweet.aggregate([
+    {"$match": {"user.screen_name":"AmericanAir"}},
+   {"$group" : {"_id":{'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}},"count": {"$sum":1}}},
+   {"$sort": {"_id": 1}},
+   {"$project": {"_id": 0, "count":1}}
+])
+for_plot3 = tweet.aggregate([
+    {"$match": {"user.screen_name":"British_Airways"}},
+   {"$group" : {"_id":{'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}},"count": {"$sum":1}}},
+   {"$sort": {"_id": 1}},
+   {"$project": {"_id": 0, "count":1}}
+])
+for_plot4 = tweet.aggregate([
+    {"$match": {"user.screen_name":"SingaporeAir"}},
+   {"$group" : {"_id":{'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}},"count": {"$sum":1}}},
+   {"$sort": {"_id": 1}},
+   {"$project": {"_id": 0, "count":1}}
+])
+index = pd.date_range(start='1/10/2018', end='1/11/2018')
+
 list_cursor = list(for_plot)
 df = DataFrame(list_cursor)
-df2 = df.set_index(index)
-#print(df2.head())
-df2['count'].plot()
-# df.plot(kind='line', x='_id', y='count')
-plt.title("Over time")
-plt.ylabel("number of tweets per day")
+
+list_cursor2 = list(for_plot2)
+df2 = DataFrame(list_cursor2)
+
+list_cursor3 = list(for_plot3)
+df3 = DataFrame(list_cursor3)
+
+list_cursor4 = list(for_plot4)
+df4 = DataFrame(list_cursor4)
+
+#"user":"$user.screen_name"
+#'hour':{"$hour":"$created_at"}
+
+df.set_index(index, inplace=True)
+df2.set_index(index, inplace=True)
+df3.set_index(index, inplace=True)
+df4.set_index(index, inplace=True)
+
+fig, ax_combo = plt.subplots(figsize=[10,8])
+df["count"].plot(x=index, ax=ax_combo)
+df2["count"].plot(x=index,ax=ax_combo)
+df3["count"].plot(x=index,ax=ax_combo)
+df4["count"].plot(x=index,ax=ax_combo)
+
+plt.legend(["KLM","Amercian_Air","British_Airways","SingaporeAir"])
+plt.title("Tweeting over time")
+plt.ylabel("Number of tweets per day")
 plt.show()
-print(df2)
+# print(df2)
+plt.savefig("airline_plot")
 
 
 
--- index.py
+++ index.py
@@ -7,3 +7,17 @@ client = pymongo.MongoClient(conn_str)
 cavemen=client.cavemen
 tweets=cavemen.tweets
 klm_conversations=cavemen.klm
+
+screen_names = ["KLM", "AirFrance", "British_Airways", "AmericanAir", "Lufthansa", "AirBerlin", "AirBerlin assist", 
+"easyJet"," RyanAir","SingaporeAir", "Qantas", "EtihadAirways", "VirginAtlantic"]
+
+for_plot = tweets.aggregate([
+    {"$match": {"user.screen_name":{"$in":screen_names}}},
+   {"$group" : {"_id":{"user":"$user.screen_name",'month':{"$month":"$created_at"}, 'dayofmon':{"$dayOfMonth":"$created_at"}},"count": {"$sum":1}}},
+   #{"$group" : {"_id":"$lang", "count": {"$sum":1}}},
+   #{ "$push": { "created_at": <value1>, ... } }
+   {"$sort": {"_id": 1}},
+   {"$project": {"_id": 1, "count":1}}
+])
+for doc in for_plot:
+    print(doc)
