d4e3a6f8b8c204510b970c26d9003f8833bdfe79
Igor Dmochowski <lemon.lemoniada@gmail.com>
2022-06-02 17:24:10 +0200

    fixed reply percentage

--- twitter_use.py
+++ twitter_use.py
@@ -1,5 +1,5 @@
 from index import tweets,klm_conversations,cavemen
-import numpy as np
+# import numpy as np
 
 klm_obj={'screen_name':'KLM','id':56377143}
 
@@ -17,22 +17,54 @@ vir_obj={'screen_name':'VirginAtlantic','id':20626359}
 
 others=[aa_obj,afr_obj,bra_obj,ala_obj,luf_obj,ezj_obj,rya_obj,sin_obj,qua_obj,eti_obj,vir_obj]
 colls=cavemen.list_collection_names()
-colls.remove('KLM');colls.remove('tweets')
+colls.remove('tweets')
 
 def reply_plot():
     tweets.create_index('entities.user_mentions')
-    klm_tag_number=klm_conversations.count_documents({'entities.user_mentions':{'$in': [klm_obj]}})
-    others_tags_number=tweets.count_documents({'entities.user_mentions':{'$in': others}})
-    klm_conversations_number=klm_conversations.count_documents({})
-
-    others_conversations_number=0
+    tags={};conversations={};percentage={}
+    others_tags=0
+    others_conversations=0
     for airline in colls:
-        airline=getattr(cavemen,airline)
-        others_conversations_number+=airline.count_documents({})
+        tags[airline]=0
+    for airline in colls:
+        conversations[airline]=0
 
-    percentage_klm=klm_conversations_number/klm_tag_number
-    percentage_others=others_conversations_number/others_tags_number
-    print(percentage_klm,percentage_others)
+    tags['KLM']=tweets.count_documents({'entities.user_mentions':{'$in': [klm_obj]}})
+    tags['AmericanAir']=tweets.count_documents({'entities.user_mentions':{'$in': [aa_obj]}})
+    tags['AirFrance']=tweets.count_documents({'entities.user_mentions':{'$in': [afr_obj]}})
+    tags['British_Airways']=tweets.count_documents({'entities.user_mentions':{'$in': [bra_obj]}})
+    tags['EtihadAirways']=tweets.count_documents({'entities.user_mentions':{'$in': [eti_obj]}})
+    tags['Lufthansa']=tweets.count_documents({'entities.user_mentions':{'$in': [luf_obj]}})
+    tags['Qantas']=tweets.count_documents({'entities.user_mentions':{'$in': [qua_obj]}})
+    tags['RyanAir']=tweets.count_documents({'entities.user_mentions':{'$in': [rya_obj]}})
+    tags['SingaporeAir']=tweets.count_documents({'entities.user_mentions':{'$in': [sin_obj]}})
+    tags['VirginAtlantic']=tweets.count_documents({'entities.user_mentions':{'$in': [vir_obj]}})
+    tags['easyJet']=tweets.count_documents({'entities.user_mentions':{'$in': [ezj_obj]}})
+
+    conversations['KLM']=getattr(cavemen,'KLM').count_documents({})
+    conversations['AmericanAir']=getattr(cavemen,'AmericanAir').count_documents({})
+    conversations['AirFrance']=getattr(cavemen,'AirFrance').count_documents({})
+    conversations['British_Airways']=getattr(cavemen,'British_Airways').count_documents({})
+    conversations['EtihadAirways']=getattr(cavemen,'EtihadAirways').count_documents({})
+    conversations['Lufthansa']=getattr(cavemen,'Lufthansa').count_documents({})
+    conversations['Qantas']=getattr(cavemen,'Qantas').count_documents({})
+    conversations['RyanAir']=getattr(cavemen,'RyanAir').count_documents({})
+    conversations['SingaporeAir']=getattr(cavemen,'SingaporeAir').count_documents({})
+    conversations['VirginAtlantic']=getattr(cavemen,'VirginAtlantic').count_documents({})
+    conversations['easyJet']=getattr(cavemen,'easyJet').count_documents({})
+
+    for airline in tags.keys():
+        percentage[airline]=conversations[airline]/tags[airline]
+        if(airline!='KLM'):
+            others_tags+=tags[airline]
+            others_conversations+=conversations[airline]
+    
+    others_percentage=others_conversations/others_tags
+    
+    print(percentage,others_percentage)
+
+
+
 
 def sentiment_plot():
     klm_conversations.create_index('depth_1.sentiment')
