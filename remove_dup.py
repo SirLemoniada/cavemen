063b558f122d14bd772ffeaa3fa0d8c6b4d71d1d
ChrisWoycik <k.j.wojcik@gmail.com>
2022-06-06 09:13:49 +0200

    nvm

--- remove_dup.py
+++ remove_dup.py
@@ -6,4 +6,4 @@ import pandas as pd
 #index = pd.date_range(start='5/22/2019', end='3/30/2020')
 # index2 = pd.date_range(start='5/22/2019', end='5/23/2019')
 # print(type(index2))
-print('huj')
+print(tweet.find({"user.screen_name":"KLM"}).explain())
