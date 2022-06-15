2a2e65a5a24f4597b9ec05822b84e8d6d6e96c17
20210967 <d.lin1@student.tue.nl>
2022-06-15 18:43:23 +0200

    new changes for restructuring code

---
+++ Code_for_augmentation/index.py
@@ -0,0 +1,22 @@
+import pymongo
+
+conn_str = 'mongodb://127.0.0.1:27017'
+
+client = pymongo.MongoClient(conn_str)
+
+cavemen=client.cavemen
+tweets=cavemen.tweets
+
+KLM_conversations = cavemen.KLM
+British_Airways_conversations = cavemen.British_Airways 
+AirFrance_conversations = cavemen.AirFrance
+AmericanAir_conversations = cavemen.AmericanAir
+Lufthansa_conversations = cavemen.Lufthansa
+AirBerlin_conversations = cavemen.AirBerlin
+easyJet_conversations = cavemen.easyJet
+RyanAir_conversations = cavemen.RyanAir
+SingaporeAir_conversations = cavemen.SingaporeAir
+Qantas_conversations = cavemen.Qantas
+EtihadAirways_conversations = cavemen.EtihadAirways
+VirginAtlantic_conversations = cavemen.VirginAtlantic
+sentiment_analysis=cavemen.sentiment
