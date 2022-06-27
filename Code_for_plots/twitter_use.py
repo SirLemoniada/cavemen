2c14db6bff55b3adfd159e64aa97164a26fc9e7c
Igor Dmochowski <lemon.lemoniada@gmail.com>
2022-06-27 22:33:23 +0200

    removed unnecessary plot from twitter-use

--- Code_for_plots/twitter_use.py
+++ Code_for_plots/twitter_use.py
@@ -22,49 +22,6 @@ vir_obj={'screen_name':'VirginAtlantic','id':20626359}
 others=[aa_obj,afr_obj,bra_obj,ala_obj,luf_obj,ezj_obj,rya_obj,sin_obj,qua_obj,eti_obj,vir_obj]
 colls=cavemen.list_collection_names()
 colls.remove('tweets')
-def reply_plot():
-    tweets.create_index('entities.user_mentions')
-    tags={};conversations={};percentage={}
-    others_tags=0
-    others_conversations=0
-    for airline in colls:
-        tags[airline]=0
-    for airline in colls:
-        conversations[airline]=0
-
-    tags['KLM']=tweets.count_documents({'entities.user_mentions':{'$in': [klm_obj]}})
-    tags['AmericanAir']=tweets.count_documents({'entities.user_mentions':{'$in': [aa_obj]}})
-    tags['AirFrance']=tweets.count_documents({'entities.user_mentions':{'$in': [afr_obj]}})
-    tags['British_Airways']=tweets.count_documents({'entities.user_mentions':{'$in': [bra_obj]}})
-    tags['EtihadAirways']=tweets.count_documents({'entities.user_mentions':{'$in': [eti_obj]}})
-    tags['Lufthansa']=tweets.count_documents({'entities.user_mentions':{'$in': [luf_obj]}})
-    tags['Qantas']=tweets.count_documents({'entities.user_mentions':{'$in': [qua_obj]}})
-    tags['RyanAir']=tweets.count_documents({'entities.user_mentions':{'$in': [rya_obj]}})
-    tags['SingaporeAir']=tweets.count_documents({'entities.user_mentions':{'$in': [sin_obj]}})
-    tags['VirginAtlantic']=tweets.count_documents({'entities.user_mentions':{'$in': [vir_obj]}})
-    tags['easyJet']=tweets.count_documents({'entities.user_mentions':{'$in': [ezj_obj]}})
-
-    conversations['KLM']=getattr(cavemen,'KLM').count_documents({})
-    conversations['AmericanAir']=getattr(cavemen,'AmericanAir').count_documents({})
-    conversations['AirFrance']=getattr(cavemen,'AirFrance').count_documents({})
-    conversations['British_Airways']=getattr(cavemen,'British_Airways').count_documents({})
-    conversations['EtihadAirways']=getattr(cavemen,'EtihadAirways').count_documents({})
-    conversations['Lufthansa']=getattr(cavemen,'Lufthansa').count_documents({})
-    conversations['Qantas']=getattr(cavemen,'Qantas').count_documents({})
-    conversations['RyanAir']=getattr(cavemen,'RyanAir').count_documents({})
-    conversations['SingaporeAir']=getattr(cavemen,'SingaporeAir').count_documents({})
-    conversations['VirginAtlantic']=getattr(cavemen,'VirginAtlantic').count_documents({})
-    conversations['easyJet']=getattr(cavemen,'easyJet').count_documents({})
-
-    for airline in tags.keys():
-        percentage[airline]=conversations[airline]/tags[airline]
-        if(airline!='KLM'):
-            others_tags+=tags[airline]
-            others_conversations+=conversations[airline]
-    
-    others_percentage=others_conversations/others_tags
-
-    print(percentage,others_percentage)
 
 
 def sentiment_evolution_plot(above_months:list,below_months:list):
