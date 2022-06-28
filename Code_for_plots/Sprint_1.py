3df35ec73959240fc3863a2cab8eed8bd22a183f
20210967 <d.lin1@student.tue.nl>
2022-06-28 10:16:41 +0200

    final changes

--- Code_for_plots/Sprint_1.py
+++ Code_for_plots/Sprint_1.py
@@ -15,12 +15,8 @@ def Number_of_tweets_per_language():
     for x, y in sorted_lang_dict.items():
         key.append(x)
         value.append(y)
-    first_four_key = key[0:4]
-    first_four_value = value[0:4]
-    first_four_value.append(sum(value[5:]))
-    first_four_key.append('others')
 
-    plt.pie(first_four_value, labels=first_four_key, autopct='%1.1f%%', colors = ['tab:orange', 'tab:cyan', 'tab:green', 'tab:purple', 'tab:gray'])
+    plt.bar(key,value)
     plt.title("Tweets per language")
     plt.show()
 
@@ -70,43 +66,28 @@ def Number_of_followers_for_each_airline():
     plt.ylabel("Number of followers")
     plt.show()
 
-def Tweets_sent_by_each_airline_per_day_in_June():
-    KLM_day_list_tmp=[]
-    British_Airways_day_list_tmp=[]
-    American_Airlines_day_list_tmp=[]
-    KLM_day_list = []
-    British_Airways_day_list=[]
-    American_Airlines_day_list=[]
-    Days = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30']
-    Days_int = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+def Tweets_sent_by_each_airline_per_week_in_June():
+    KLM_week = {22:0, 23:0, 24:0, 25:0}
+    British_Airways_week = {22:0, 23:0, 24:0, 25:0}
+    American_Airlines_week = {22:0, 23:0, 24:0, 25:0}
 
-    for date in tweet.find({"created_at": {"$exists" : True}}):
-        if date['created_at'].split()[1] == 'Jun':
-            if date['user']['id'] == 56377143:
-                splited_string = date['created_at'].split()
-                KLM_day_list_tmp.append(splited_string[2])
-            elif date['user']['id'] == 18332190:
-                splited_string = date['created_at'].split()
-                British_Airways_day_list_tmp.append(splited_string[2])
-            elif date['user']['id'] == 22536055:
-                splited_string = date['created_at'].split()
-                American_Airlines_day_list_tmp.append(splited_string[2])
+    for week in range(22,26):
+        for each_tweet in tweet.find({"$expr": { "$eq": [{ "$week": "$created_at" }, week]}, "user.id": {"$in" : [56377143,18332190,22536055]}}):
+            if each_tweet['user']['id'] == 56377143:
+                KLM_week[week] += 1
+            elif each_tweet['user']['id'] == 18332190:
+                British_Airways_week[week] += 1
+            elif each_tweet['user']['id'] == 22536055:
+                American_Airlines_week[week] += 1
 
-    for day in Days:
-        KLM_day_list.append(KLM_day_list_tmp.count(day))
-        British_Airways_day_list.append(British_Airways_day_list_tmp.count(day))
-        American_Airlines_day_list.append(American_Airlines_day_list_tmp.count(day))
-
-    plt.plot(Days_int, KLM_day_list, label = 'KLM')
-    plt.plot(Days_int, British_Airways_day_list, label = 'British Airways')
-    plt.plot(Days_int, American_Airlines_day_list, label = 'American Airlines', color = 'purple')
-    plt.title("Tweets sent by each airline per day in June")
-    plt.legend()
-    plt.xlabel("Day")
-    plt.ylabel("Number of tweets")
+    fig, axes = plt.subplots(3, 1, sharex = True)
+    axes[0].bar(*zip(*KLM_week.items()))
+    axes[0].set_title('KLM', size = 15)
+    axes[1].bar(*zip(*British_Airways_week.items()))
+    axes[1].set_title('British_Airways', size = 15)
+    axes[2].bar(*zip(*American_Airlines_week.items()), label = 'American Airlines')
+    axes[2].set_title('American Airlines', size = 15)
+    fig.suptitle("Tweets sent by each airline per week in June")
+    fig.supxlabel("Week")
+    fig.supylabel("Number of tweets")
     plt.show()
-
-# Number_of_tweets_per_language()
-# Number_of_tweets_sent_by_each_airline()
-# Number_of_followers_for_each_airline()
-# Tweets_sent_by_each_airline_per_day_in_June()
